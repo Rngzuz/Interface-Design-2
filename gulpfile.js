e5253a576b7844a25b3eb217b550d84bb1012a31
Tobias <cchawps@gmail.com>
2017-03-20 22:01:52 +0100

    Grid initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
+'use strict';
 var gulp = require('gulp');
 var pump = require('pump');
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 
+//string|string[] of path(s) to file(s)
+var files = 'style/*.scss';
+
 gulp.task('style', function () {
     pump([
-        gulp.src('css/*.css'),
+        gulp.src(files),
         sourcemaps.init(),
+        sass.sync({
+            indentWidth: 4,
+            indentType: 'space',
+            outputStyle: 'expanded'
+        }).on('error', sass.logError),
         autoprefixer({
             browsers: ['last 2 versions'],
             cascade: true
@@ -23,5 +33,5 @@ gulp.task('default', [
 ]);
 
 gulp.task('watch', function () {
-    gulp.watch('css/*.css', ['style']);
+    gulp.watch(files, ['style']);
 });
