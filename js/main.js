3ac873ba08dba3a049a47528e2823dbaef693c53
Tobias <cchawps@gmail.com>
2017-05-01 02:37:06 +0200

    routing added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
-(function () {
-    var toggle = document.querySelector('#toggle');
+function load(element, url) {
+    var xhr = new XMLHttpRequest();
 
-    toggle.addEventListener('click', function () {
-        this.classList.toggle('active');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            wrap.innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+(function () {
+    document.addEventListener('click', function (event) {
+        var cssClass = event.target.getAttribute('data-toggle-class') || null;
+
+        if (cssClass !== null) {
+            event.target.classList.toggle(cssClass);
+        }
     });
+
+    var header = document.querySelector('#header');
+    var wrap = document.querySelector('#wrap');
+
+    var MyRouter = new Router();
+
+    MyRouter.register({
+        title: 'Search',
+        path: '/',
+        callback: function (state) {
+            document.title = state.title;
+            header.classList.remove('red', 'green');
+            header.classList.add('teal');
+
+            load(wrap, '/search.html');
+        }
+    });
+
+    MyRouter.register({
+        title: 'Locations',
+        path: '/locations',
+        callback: function (state) {
+            document.title = state.title;
+            header.classList.remove('red', 'teal');
+            header.classList.add('green');
+
+            load(wrap, '/locations.html');
+        }
+    });
+
+    MyRouter.init();
 })();
---
+++ js/router.js
@@ -0,0 +1,48 @@
+var Router = function () {
+    var _this = this;
+    this.paths = new Map();
+    this.current = window.location.pathname;
+
+    window.addEventListener('popstate', function (event) {
+        var state = _this.paths.get(event.state) || null;
+
+        if (state !== null) {
+            state.callback(state);
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        var target = event.target.getAttribute('data-navigate') || null;
+
+        if (target !== null) {
+            event.preventDefault();
+            _this.navigate(target);
+        }
+    });
+};
+
+Router.prototype.register = function (state) {
+    this.paths.set(state.path, state);
+};
+
+Router.prototype.navigate = function (path) {
+    var state = this.paths.get(path);
+
+    if (state && path !== this.current) {
+        this.current = path;
+        window.history.pushState(state.path, state.title, state.path);
+        state.callback(state);
+    } else if (path !== this.current) {
+        console.log('404');
+    }
+};
+
+Router.prototype.init = function () {
+    var state = this.paths.get(window.location.pathname);
+
+    if (state) {
+        state.callback(state);
+    } else {
+        console.log('404');
+    }
+};
